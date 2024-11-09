2384c0e90318d11d59a8fad2c3f522b93f0e4f3f
nara <parknara0213@gmail.com>
2024-11-09 23:32:57 +0900

    Feat: 추가 testing rendering

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,24 @@
+import { it, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Greet from "../../src/components/Greet";
+import "@testing-library/jest-dom/vitest";
+
+describe("Greet", () => {
+  it("should render Hello with the name when name is provided", () => {
+    render(<Greet name="Mosh" />);
+
+    // screen.debug();
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/hello mosh/i);
+  });
+
+  it("should render login button when nam is not provided", () => {
+    render(<Greet />);
+
+    // screen.debug();
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
